7a643eab2e113e77e69830bd48405855524fcac7
adrianprzcb <adrianpc4444@gmail.com>
2026-09-26 00:56:45 +0200

    MSV Inventory -> Se añade desde Spring Initializer.

---
+++ backend/inventory/src/test/java/com/adrianperezcobo/dummycommerce/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianperezcobo.dummycommerce.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
